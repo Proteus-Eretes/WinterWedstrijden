10f1d0b94c5f090602e89cc99f92d97d2639e675
Dany Sluijk <me@dany.dev>
2022-11-16 02:30:34 +0100

    feat: add international page

--- pages/international.vue
+++ pages/international.vue
@@ -1,11 +1,279 @@
 <template>
   <v-card flat light>
     <v-card-title>
-      International
+      International Information
     </v-card-title>
     <v-card-text>
-      This page will be filled later with international information. Come back
-      later!
+      <h1>General Information</h1>
+      <h2>Entries</h2>
+      <p>
+        On the 18th and 19th of February 2023 the 48th Van Oord
+        WinterWedstrijden Regatta will take place on the river Schie between
+        Rotterdam and Delft, The Netherlands. This is the first regatta of the
+        national rowing season and we would be very happy to receive
+        participating teams from all over the world. Entries for international
+        crews will be open from the 9th of January until the 30th of January
+        2023.
+      </p>
+      <p>
+        To participate, send an email to
+        <a
+          href="mailto:wedstrijdleiding.winterwedstrijden@proteus-eretes.nl"
+          title="Wedstrijdleiding"
+          target="_blank"
+          >wedstrijdleiding.winterwedstrijden@proteus-eretes.nl</a
+        >
+        with the following information:
+      </p>
+      <ul>
+        <li>The full name of your rowing club</li>
+        <li>Address</li>
+        <li>City</li>
+        <li>Country</li>
+        <li>Contact name</li>
+        <li>Contact mail address</li>
+        <li>Contact telephone number</li>
+        <li>Small description of blade, shirt and trousers</li>
+      </ul>
+      <h2>Pasta dinner</h2>
+      <p>
+        On Friday we organise a free pasta dinner for foreign crews at the
+        boathouse of Proteus-Eretes (Rotterdamseweg 362a, Delft). Make sure you
+        do not miss it!
+      </p>
+      <h2>Accommodation</h2>
+      <p>
+        We do not have special deals with hostels or hotels, but if you would
+        like to receive any recommendations regarding accommodation, please
+        contact
+        <a
+          href="mailto:info.winterwedstrijden@proteus-eretes.nl"
+          title="WW Info"
+          target="_blank"
+          >info.winterwedstrijden@proteus-eretes.nl</a
+        >.
+      </p>
+      <h2>Registration Fees</h2>
+      <ul>
+        <li>€120,- for an 8+.</li>
+        <li>€78,- for a 4x and 4+.</li>
+        <li>€62,- for a 2x and 2-.</li>
+        <li>€36,- for a 1x.</li>
+      </ul>
+      <h2>Boat Rental</h2>
+      <p>
+        If possible, we kindly ask you to bring your own boat and oars. Often
+        boat transport can be combined with other participating crews. Crews who
+        are not able to bring their own boat and oars can contact
+        <a
+          href="mailto:info.winterwedstrijden@proteus-eretes.nl"
+          title="WW Info"
+          target="_blank"
+          >info.winterwedstrijden@proteus-eretes.nl</a
+        >
+        for more information about the possibilities and rental fees. Note that
+        there are a limited number of rental boats available.
+      </p>
+      <p>
+        Please note we do not arrange a cox or cox box. You have to sign a
+        “declaration of own risk” before using the boat.
+      </p>
+      <h2>Registration boat trailers</h2>
+      <p>If you bring your own boat, you need to register your boat trailer.</p>
+      <p>
+        Please send an email to
+        <a
+          href="mailto:wedstrijdleiding.winterwedstrijden@proteus-eretes.nl"
+          title="Wedstrijdleiding"
+          target="_blank"
+          >wedstrijdleiding.winterwedstrijden@proteus-eretes.nl</a
+        >
+        with the following information:
+      </p>
+      <ul>
+        <li>Arrival time and date</li>
+        <li>Name of the Boat trailer</li>
+        <li>(if possible) name of driver and phone number</li>
+        <li>How many boats, including types per trailer</li>
+      </ul>
+      <h2>Payment instruction</h2>
+      <p>
+        Instructions will be sent to you by email after filing your entry. Your
+        registration is only final after we receive payment of the registration
+        (and rental) fees.
+      </p>
+      <h2>Information during the regatta</h2>
+      <p>
+        Information will be shared via the homepage of the website. We also
+        provide information via a whatsapp service. This service is not yet
+        available, but comes soon.
+      </p>
+      <h1>Time Schedule</h1>
+      <h2>Saturday February 18th 2023</h2>
+      <v-simple-table>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Shift
+            </th>
+            <th class="text-left">
+              Time
+            </th>
+            <th class="text-left">
+              Fields
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Shift 1</td>
+            <td>10:00 - 12:00</td>
+            <td>
+              LM 2x, LW 1x
+            </td>
+          </tr>
+          <tr>
+            <td>Shift 2</td>
+            <td>13:00 - 15:00</td>
+            <td>
+              M 2x, M 2-, W 1x
+            </td>
+          </tr>
+          <tr>
+            <td>Shift 3</td>
+            <td>16:00 - 18:00</td>
+            <td>M 8+, LM 8+, W 4+</td>
+          </tr>
+        </tbody>
+      </v-simple-table>
+      <h2>Sunday February 19th 2023</h2>
+      <v-simple-table>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Shift
+            </th>
+            <th class="text-left">
+              Time
+            </th>
+            <th class="text-left">
+              Fields
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Shift 4</td>
+            <td>10:00 - 12:00</td>
+            <td>
+              W 2x, LM 1x
+            </td>
+          </tr>
+          <tr>
+            <td>Shift 5</td>
+            <td>13:00 - 15:00</td>
+            <td>
+              LW 2x, W 2-, M 1x
+            </td>
+          </tr>
+          <tr>
+            <td>Shift 6</td>
+            <td>16:00 - 18:00</td>
+            <td>W 8+, M 4+</td>
+          </tr>
+        </tbody>
+      </v-simple-table>
+      <h1>Coxswains and steering bows</h1>
+      <h2>Race instructions</h2>
+      <p>
+        The following presentation will give you information on the rules that
+        apply during the Van Oord Winterwedstrijden regatta.
+      </p>
+      <ul>
+        <li>
+          The instruction will emphasise some of the important aspects that you
+          will encounter during the race.
+        </li>
+        <li>
+          The overtaking Instructions will emphasise several situations on how
+          to pass each other obeying the rules.
+        </li>
+      </ul>
+      <h2>Coxswain weighing</h2>
+      <ul>
+        <li>
+          For the rules on the weight of the coxswains, we refer to the Dutch
+          Rules of Racing, Article 7, sub3. (only in Dutch)
+        </li>
+        <li>
+          Coxswain weighing will take place at rowingclub Proteus-Eretes
+          (Rotterdamseweg 362, Delft). Like the rowers, also the coxswains
+          weighing will take place between 2 and 1 hours before the start of
+          their own devision.
+        </li>
+        <li>
+          No weighing of coxswains for master, mixed, corporate and junior
+          events.
+        </li>
+        <li>Coxswains must be weighed wearing their racing uniform.</li>
+        <li>Coxswains must provide their own deadweight if needed.</li>
+      </ul>
+      <h2>Coxswain age</h2>
+      <p>
+        <b>Attention:</b> The Dutch Rules of Rowing state that the minimum age
+        of the coxswain needs to be 12 years.
+      </p>
+      <h2>Race course</h2>
+      <p>Will be added later</p>
+      <h1>Weather Codes</h1>
+      <p>
+        The Van Oord WinterWedstrijden regatta uses weather codes. As from
+        Wednesday before the race weekend, 20.00h, the up to date weather codes
+        will be presented at the home page.
+      </p>
+      <p>
+        There are three weather codes: Green, Yellow and Red. These codes
+        indicate the situation for the first following time slot.
+      </p>
+      <p>The weather codes will be communicated on the homepage.</p>
+      <p>
+        The decision if a time slot will be cancelled or continued will be made
+        at least 1 hour and 30 minutes before the first start of each time slot.
+        From that moment the colour green or red will be featured. This means
+        the decision will be made at:
+      </p>
+      <ul>
+        <li>Time slot 1: Saturday, 08:30</li>
+        <li>Time slot 2: Saturday, 11:30</li>
+        <li>Time slot 3: Saturday, 14:15</li>
+        <li>Time slot 4: Sunday, 08:30</li>
+        <li>Time slot 5: Sunday, 11:30</li>
+        <li>Time slot 6: Sunday, 14:15</li>
+      </ul>
+      <h2>Explanation weather codes</h2>
+      <p>
+        <b>Green:</b> The weather is not indicating any reason to change
+        anything in the schedule.
+      </p>
+      <p>
+        <b>Yellow:</b> Pay attention, the weather may result in postponing or
+        cancelling the next time slot. Please keep an eye on the website.
+      </p>
+      <p>
+        <b>Red:</b> The weather is not allowing us to continue with the
+        competition without compromising the safety of rowers and staff. The
+        next time slot is cancelled until further orders. Please keep an eye on
+        the website for further information.
+      </p>
     </v-card-text>
   </v-card>
 </template>
+
+<style scoped>
+h1,
+h2,
+h3 {
+  padding-top: 0.2em;
+  padding-bottom: 0.4em;
+}
+</style>
