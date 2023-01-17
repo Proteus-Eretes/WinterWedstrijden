9a70227deaad8e4960d94a3ad909947bcd5fbf19
DaniqueBuchner <daniquebuchner@hotmail.com>
2023-01-17 22:30:31 +0100

    nieuws op homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
+    <v-card light>
+            <v-card-title>48ste van Oord WinterWedstrijden</v-card-title>
+            <v-card-text color="black"> Vijf kilometer varen in een achtervolgingsrace op de Schie in
+              winterse weersomstandigheden: dat zijn de Van Oord WinterWedstrijden.
+              Deze officiële opening van het nationale roeiseizoen zal dit jaar 18 en 19 februari plaatsvinden.
+            </br>
+              Iedereen zal hier gaan strijden voor een goede start van hun seizoen. Wat voor de eerstejaarsploegen
+              de eerste echte roeiwedstrijd zal worden, zal voor de toppers van Nederland het moment zijn om zich
+              te bewijzen op dit meetmoment van de KNRB. Maar wat iedereen gemeen heeft: het zal een harde, koude en
+              epische race worden!</v-card-text>
+          </v-card>
     <v-card flat light v-for="item of nieuwsItems" :key="item.id">
       <div class="d-flex flex-wrap flex-md-nowrap justify-space-between">
         <div>
+
           <v-card-title>{{ item.title }}</v-card-title>
           <v-card-text v-dompurify-html="item.content" />
         </div>
