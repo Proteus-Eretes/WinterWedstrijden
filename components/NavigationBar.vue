9d59e4c929256737d2fcd9a51ada7ae8e50fb5c8
Ashmorra <74253388+Ashmorra@users.noreply.github.com>
2023-02-01 18:25:41 +0100

    Update NavigationBar.vue

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -2,40 +2,22 @@
   <div>
     <v-navigation-drawer v-model="drawerOpen" clipped absolute>
       <v-list flat>
-        <v-btn to="/" text width="100%" class="drawer--item">
-          Nieuws
-        </v-btn>
-        <v-btn to="/velden" text width="100%" class="drawer--item">
-          Deelnemers
-        </v-btn>
-        <v-btn to="/bepalingen" text width="100%" class="drawer--item"> Bepalingen </v-btn>
-        <v-btn to="/geschiedenis" text width="100%" class="drawer--item">
-          Geschiedenis
-        </v-btn>
-        <v-btn to="/blokken" text width="100%" class="drawer--item">
-          Blokken
-        </v-btn>
+        <v-btn to="/" text width="100%" class="drawer--item">Nieuws</v-btn>
+        <v-btn to="/velden" text width="100%" class="drawer--item">Deelnemers</v-btn>
+        <v-btn to="/bepalingen" text width="100%" class="drawer--item">Bepalingen</v-btn>
+        <v-btn to="/geschiedenis" text width="100%" class="drawer--item">Geschiedenis</v-btn>
+        <v-btn to="/blokken" text width="100%" class="drawer--item">Blokken</v-btn>
         <v-btn
           target="_blank"
           href="https://hoesnelwasik.nl/ww/0/uitslagen#blocks"
           text
           width="100%"
           class="drawer--item"
-        >
-          Uitslagen
-        </v-btn>
-        <v-btn href="https://www.proteus-eretes.nl/fotos" text width="100%" class="drawer--item">
-          Foto's
-        </v-btn>
-        <v-btn to="/vanoord" text width="100%" class="drawer--item">
-          Van Oord
-        </v-btn>
-        <v-btn to="/contact" text width="100%" class="drawer--item">
-          Contact
-        </v-btn>
-        <v-btn to="/international" text width="100%" class="drawer--item">
-          International
-        </v-btn>
+        >Uitslagen</v-btn>
+        <v-btn href="https://www.proteus-eretes.nl/fotos" text width="100%" class="drawer--item">Foto's</v-btn>
+        <v-btn to="/vanoord" text width="100%" class="drawer--item">Van Oord</v-btn>
+        <v-btn to="/contact" text width="100%" class="drawer--item">Contact</v-btn>
+        <v-btn to="/international" text width="100%" class="drawer--item">International</v-btn>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar dark color="#131b42ff" flat>
