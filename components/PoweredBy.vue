7fe40f2cdc13f5a87c6eaea78131296cc9608d6f
Ashmorra <74253388+Ashmorra@users.noreply.github.com>
2023-02-01 16:08:36 +0100

    Update PoweredBy.vue

--- components/PoweredBy.vue
+++ components/PoweredBy.vue
@@ -3,7 +3,17 @@
     <v-card-title>
       Powered by
     </v-card-title>
+    
     <div class="d-flex flex-wrap flex-sm-nowrap flex-lg-wrap">
+      <v-img src="/pe.png" />
+      <v-img src="/vanoord.png" />
+      <v-img src="/tu-delft.png" />
+      <v-img src="/jupiler.jpg" />
+      <v-img src="/roeigoed.jpeg" />
+      <v-img src="/iris.png" />
+    </div>
+
+    <!-- <div class="d-flex flex-wrap flex-sm-nowrap flex-lg-wrap">
       <v-img src="/pe.png" />
       <v-img src="/vanoord.png" />
       <v-img src="/tu-delft.png" />
@@ -12,7 +22,8 @@
       <v-img src="/jupiler.jpg" />
       <v-img src="/roeigoed.jpeg" />
       <v-img src="/iris.png" />
-    </div>
+    </div> -->
+
   </v-card>
 </template>
 
