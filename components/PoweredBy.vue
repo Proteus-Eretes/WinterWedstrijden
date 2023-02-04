606c87b5f0d5d149e4a66c86ec0d8d50c9fcde31
Ashmorra <74253388+Ashmorra@users.noreply.github.com>
2023-02-04 17:41:42 +0100

    Update PoweredBy.vue

--- components/PoweredBy.vue
+++ components/PoweredBy.vue
@@ -4,17 +4,16 @@
     <v-card-title>
       Weercode
     </v-card-title>
-    <div class="d-flex flex-wrap flex-sm-nowrap flex-lg-wrap">
+    <v-card-text class="d-flex flex-wrap flex-sm-nowrap flex-lg-wrap">
       Groen&nbsp;<span class="dot" style="background-color: LimeGreen"></span>
       <!-- Geel&nbsp;<span class="dot" style="background-color: gold"></span> -->
       <!-- Rood&nbsp;<span class="dot" style="background-color: red"></span> -->
       <br>Laatste update: 4 feb 2023, 18:00
-    </div>
+    </v-card-text>
     
     <v-card-title>
       Powered by
-    </v-card-title>
-    
+    </v-card-title>  
     <div class="d-flex flex-wrap flex-sm-nowrap flex-lg-wrap">
       <v-img src="/pe.png" />
       <v-img src="/vanoord.png" />
@@ -23,17 +22,6 @@
       <v-img src="/roeigoed.jpeg" />
       <v-img src="/iris.png" />
     </div>
-
-    <!-- <div class="d-flex flex-wrap flex-sm-nowrap flex-lg-wrap">
-      <v-img src="/pe.png" />
-      <v-img src="/vanoord.png" />
-      <v-img src="/tu-delft.png" />
-    </div>
-    <div class="d-flex flex-wrap flex-sm-nowrap flex-lg-wrap">
-      <v-img src="/jupiler.jpg" />
-      <v-img src="/roeigoed.jpeg" />
-      <v-img src="/iris.png" />
-    </div> -->
 
   </v-card>
 </template>
