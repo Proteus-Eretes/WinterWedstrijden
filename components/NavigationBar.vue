e800ef9fa356db09778661f974ece44ba3d23dfe
Ashmorra <74253388+Ashmorra@users.noreply.github.com>
2023-02-10 18:51:52 +0100

    Update NavigationBar.vue

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -13,7 +13,7 @@
         <v-btn href="https://hoesnelwasik.nl/ww/0/uitslagen#blocks" target="_blank" text width="100%" class="drawer--item">Uitslagen</v-btn>
         <v-btn to="/baanrecords" text width="100%" class="drawer--item">Baanrecords</v-btn>
         <v-btn href="https://www.proteus-eretes.nl/fotos" target="_blank" text width="100%" class="drawer--item">Foto's</v-btn>
-        <v-btn to="/vanoord" text width="100%" class="drawer--item">Van Oord</v-btn>
+        <v-btn to="/van%20oord" text width="100%" class="drawer--item">Van Oord</v-btn>
         <v-btn to="/contact" text width="100%" class="drawer--item">Contact</v-btn>
         <v-btn to="/international" text width="100%" class="drawer--item">International</v-btn>
       </v-list>
@@ -98,7 +98,7 @@
 
         <v-btn href="https://hoesnelwasik.nl/ww/0/uitslagen#blocks" target="_blank" text dark>Uitslagen</v-btn>
         <v-btn href="https://www.proteus-eretes.nl/fotos" target="_blank" text dark>Foto's</v-btn>
-        <v-btn to="/vanoord" text dark>Van Oord</v-btn>
+        <v-btn to="/van%20oord" text dark>Van Oord</v-btn>
         <v-btn to="/contact" text dark>Contact</v-btn>
         <v-btn to="/international" text dark>International</v-btn>
       </v-toolbar-items>
