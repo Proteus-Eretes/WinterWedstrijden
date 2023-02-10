808d1194a87d720c7d18608ffbde6b4b0307327a
Ashmorra <74253388+Ashmorra@users.noreply.github.com>
2023-02-10 18:48:15 +0100

    Create van oord.vue

---
+++ pages/van oord.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card flat light>
+    <v-card-title>
+      Van Oord Marine Ineguity
+    </v-card-title>
+    <div class="video">
+      <client-only>
+        <vimeo-player ref="player" :video-id="159516344" />
+      </client-only>
+    </div>
+    <v-card-text>
+      Van Oord is een wereldwijd actieve en toonaangevende aannemer van bagger-,
+      waterbouwkundige en offshore projecten (olie, gas en wind). Wij zijn een
+      innovatieve partner voor onze opdrachtgevers en dragen al 150 jaar bij aan
+      de infrastructuur voor de wereld van morgen.
+      <br />
+      <br />
+      De groeiende wereldbevolking heeft behoefte aan meer ruimte en de vraag
+      naar energie stijgt voortdurend. Toenemende wereldhandel vraagt om meer en
+      betere havenfaciliteiten, terwijl klimaatverandering kustgebieden
+      bedreigt. Van Oord biedt innovatieve oplossingen voor deze wereldwijde
+      maritieme uitdagingen; nu en in de toekomst.
+      <br />
+      <br />
+      Van Oord is een in Nederland gevestigde, zelfstandige familieonderneming,
+      die zich kenmerkt door zichtbaar leiderschap, langetermijnvisie en solide
+      financiering. Aanzienlijke investeringen in mensen en materieel worden met
+      grote zorg gedaan om de continuïteit van onze onderneming te waarborgen.
+      Onze mensen zijn betrokken, ondernemende professionals met een passie voor
+      water en technologie. Zij kennen het belang van samenwerken voor het
+      bereiken van het best mogelijke resultaat. Door open en integer te zijn,
+      bouwen zij aan wederzijds vertrouwen en respect.
+      <br />
+      <br />
+      Bekijk de website van Van Oord
+      <a
+        href="https://www.vanoord.com/"
+        title="Van Oord"
+        rel="noopener"
+        target="_blank"
+        >hier</a
+      >.
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped>
+/* Video */
+.video {
+  width: 100%;
+  max-width: 90vw;
+  display: flex;
+  justify-content: center;
+}
+
+.video iframe {
+  width: 100% !important;
+}
+</style>
