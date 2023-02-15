8ddcf9e79b3d8204b650284532d8e8c04034853c
Ashmorra <74253388+Ashmorra@users.noreply.github.com>
2023-02-15 14:44:24 +0100

    Update fietsverhuur.vue

--- pages/fietsverhuur.vue
+++ pages/fietsverhuur.vue
@@ -3,12 +3,21 @@
       <v-card-title>
         Fietsverhuur tijdens de WinterWedstrijden
       </v-card-title>
-      <v-card-text>
-        <p>
-            Tijdens de wedstrijd zijn er fietsen te huur voor coaches en Toeschouwers
-            om mee te fietsen langs de baan. De fietsen zijn te huur per blok of voor
-            een hele dag.
-        </p>
+      <v-card-text color="black">
+          Tijdens de wedstrijd zijn er fietsen te huur voor coaches en Toeschouwers
+          om mee te fietsen langs de baan. De fietsen zijn te huur per blok of voor
+          een hele dag voor de volgende prijs:
+          <ul>
+            <li>€4,50 voor een blok</li>
+            <li>€10 voor de hele dag</li>
+          </ul>
+          <b>
+            Let op dat er alleen gepind kan worden
+          </b>
+          <br>
+          De fietsen worden verhuurd op het terrein van Lijm en Cultuur.
+          Let er wel op dat de fietsen tijdig worden teruggebracht om extra
+          om extra kosten te vermijden.
       </v-card-text>
     </v-card>
   </template>
