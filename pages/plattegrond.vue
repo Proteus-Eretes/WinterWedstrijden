a36f414ce12adb7ead5056c20fe948541dfa5a5f
Ashmorra <74253388+Ashmorra@users.noreply.github.com>
2023-02-15 17:32:41 +0100

    plattegronden toevoegen

---
+++ pages/plattegrond.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-card flat light>
+      <v-card-title>
+        Plattegrond Proteus-Eretes
+      </v-card-title>
+      <v-card-text>
+        <b>De weging vindt plaats op de ergozolder van Proteus-Eretes.
+        Hier kunt u komen via de loodsdeur op het voorterrein.</b>
+        Het adres van Proteus-Eretes:<br>
+        <b>De Beuk</b><br>
+        Rotterdamseweg 362A<br>
+        2628 AT<br>
+        Delft
+        <img src="/plattegrondpe.png" class="baankaart" />
+      </v-card-text>
+      <v-card-title>
+        Plattegrond Lijm & Cultuur
+      </v-card-title>
+      <v-card-text>
+        Het adres van Lijm & Cultuur:<br>
+        <b>De Beuk</b><br>
+        Rotterdamseweg 272<br>
+        2628 AT<br>
+        Delft
+        <img src="/plattegrondlc.png" class="baankaart" />
+      </v-card-text>
+    </v-card>
+  </template>
+  
+  <style scoped>
+  h1,
+  h2,
+  h3 {
+    padding-top: 0.2em;
+    padding-bottom: 0.4em;
+  }
+  
+  .baankaart {
+    display: block;
+    max-width: 460px;
+    margin: 0 auto;
+  }
+  </style>
+  
