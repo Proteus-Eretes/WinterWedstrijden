b97313d461196aff19c5229d8531ad1339435b80
Ashmorra <74253388+Ashmorra@users.noreply.github.com>
2023-02-21 16:51:46 +0100

    merchandise

---
+++ pages/merchandise.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-card flat light>
+      <v-card-title>
+        WinterWedstrijden merchandise
+      </v-card-title>
+      <v-card-text>
+        In samenwerking met PowerHouse Sport hebben wij merchandise van de 48e Van 
+        Oord WinterWedstrijden ontwikkeld. In de 
+        <a href="https://www.powerhousesport.nl/winterwedstrijden/" target="_blank">webshop</a> 
+        vind je een roeipakje, waarvan de vrouwelijke een racersback heeft, een sweater 
+        en een canvastas. De webshop is geopend tot en met 22 februari, dus wees er snel bij!
+        <img src="/merch1.jpg" class="baankaart" />
+        <img src="/merch2.jpg" class="baankaart" />
+        <img src="/merch3.jpg" class="baankaart" />
+      </v-card-text>
+    </v-card>
+  </template>
+  
+  <style scoped>
+  h1,
+  h2,
+  h3 {
+    padding-top: 0.2em;
+    padding-bottom: 0.4em;
+  }
+  
+  .baankaart {
+    display: block;
+    max-width: 50%;
+    margin: 0 auto;
+  }
+  </style>
+  
