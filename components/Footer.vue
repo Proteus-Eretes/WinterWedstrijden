7406ed13d9345f05938776a645577a3fc173d542
WWK2024 <promo.winterwedstrijden@proteus-eretes.nl>
2023-09-20 12:13:22 +0200

    weer terug veranderd
    weer terug veranderd

--- components/Footer.vue
+++ components/Footer.vue
@@ -107,14 +107,9 @@
       </div>
       <div class="col-md-4">
         <h3>Georganiseerd door leden van</h3>
-        <v-img class="logo" src="/pew.png" contain height="50px"/>
+        <v-img src="/pew.png" contain height="50px"/>
       </div>
     </v-row>
   </v-container>
 </template>
 
-<style scoped>
-.logo {
-  margin: 0;
-}
-</style>
