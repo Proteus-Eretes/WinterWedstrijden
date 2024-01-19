9bdaea568b9454fccb79644490ea1a99dfb5f57c
WWK2024 <promo.winterwedstrijden@proteus-eretes.nl>
2024-01-19 12:42:17 +0100

    enter toegevoegd aan v-card

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <v-card light>
             <v-card-title>Wijzigingen blokschema</v-card-title>
             <v-card-text color="black"> LET OP! De blokindeling is gewijzigd, dus controleer of er voor jou of jouw roeiers iets is veranderd. 
+              <br>
               Na overleg met de KNRB is besloten dat de junioren ook zullen deelnemen aan de 5k-wedstrijd. Dit besluit is genomen vanwege het dubbel starten en het gezamenlijk gebruik van boten.</v-card-text>
     </v-card>
     <br>
