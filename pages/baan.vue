375d484fcb382e239ee7248eebfef8e5123a5c9f
WWK2024 <promo.winterwedstrijden@proteus-eretes.nl>
2024-01-31 15:09:40 +0100

    baankaart 3k weg

--- pages/baan.vue
+++ pages/baan.vue
@@ -12,20 +12,7 @@
         </ul>
         <img src="/baankaart5k.png" class="baankaart" />
       </v-card-text>
-      <v-card-title>
-        Baankaart 3000 meter
-      </v-card-title>
-      <v-card-text>
-        <ul>
-            <li>Start ten noorden van de Kandelaarbrug</li>
-            <li>Finish ten zuiden van de Kruithuisbrug</li>
-            <li>Lage nummers starten eerst</li>
-            <li>Vliegende start</li>
-        </ul>
-        <img src="/baankaart3k.png" class="baankaart" />
-      </v-card-text>
-    </v-card>
-  </template>
+
   
   <style scoped>
   h1,
