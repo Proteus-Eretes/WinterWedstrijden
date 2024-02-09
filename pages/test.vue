a52fbb1e13a4b8d5752604850f7c1be0ef02ea18
Ashmorra <74253388+Ashmorra@users.noreply.github.com>
2024-02-09 18:12:12 +0100

    Update test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -25,38 +25,5 @@
               te bewijzen op dit meetmoment van de KNRB. Maar wat iedereen gemeen heeft: het zal een harde, koude en
               epische race worden!</v-card-text>
     </v-card>
-    <v-card flat light v-for="item of nieuwsItems" :key="item.id">
-      <div class="d-flex flex-wrap flex-md-nowrap justify-space-between">
-        <div>
-
-          <v-card-title>{{ item.title }}</v-card-title>
-          <v-card-text v-dompurify-html="item.content" />
-        </div>
-        <img
-          class="ma-3 flex-lg-grow-0"
-          style="max-width: 300px"
-          width="100%"
-          :src="
-            item.photoMetaData
-              ? item.photoMetaData.MEDIUM
-              : '/commissies/geenfoto.png'
-          "
-        />
-      </div>
-    </v-card>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      nieuwsItems: []
-    };
-  },
-  async asyncData({ $axios }) {
-    const res = await $axios.get("/v1/news/items?filter[category]=30");
-    return { nieuwsItems: res.data.paginate.data };
-  }
-};
-</script>
