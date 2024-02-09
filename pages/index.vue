af6b1dec477a770c52721a7cbe5371ce3d497af9
WWK2024 <promo.winterwedstrijden@proteus-eretes.nl>
2024-02-10 00:06:57 +0100

    er was dubbele v-card met zelfde tekst

--- pages/index.vue
+++ pages/index.vue
@@ -23,13 +23,6 @@
     </v-card>
     <br>
     <v-card light>
-            <v-card-title>Wijzigingen blokschema</v-card-title>
-            <v-card-text color="black"> LET OP! De blokindeling is gewijzigd, dus controleer of er voor jou of jouw roeiers iets is veranderd. 
-              <br>
-              Na overleg met de KNRB is besloten dat de junioren ook zullen deelnemen aan de 5k-wedstrijd. Dit besluit is genomen vanwege het dubbel starten en het gezamenlijk gebruik van boten.</v-card-text>
-    </v-card>
-    <br>
-    <v-card light>
             <v-card-title>49ste Van Oord WinterWedstrijden</v-card-title>
             <v-card-text color="black"> Vijf kilometer varen in een achtervolgingsrace op de Schie in
               winterse weersomstandigheden: dat zijn de Van Oord WinterWedstrijden.
