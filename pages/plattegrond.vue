707467ee522453a72e9b900f148dd04bb2dfaac3
WWK2024 <promo.winterwedstrijden@proteus-eretes.nl>
2024-02-13 14:51:10 +0100

    toevoegen parkeren

--- pages/plattegrond.vue
+++ pages/plattegrond.vue
@@ -1,4 +1,27 @@
 <template>
+  <v-card flat light>
+      <v-card-title>
+        Parkeren 
+      </v-card-title>
+      <v-card-text>
+        Wanneer u van plan bent om met de auto naar de wedstrijd te komen is het niet mogelijk om de auto op het terrein te parkeren. 
+        De beste optie is een parkeergarage op 300 meter lopen van Lijm en Cultuur. Dit is TU Delft Parking P5 met als adres: Cornelis Drebbelweg 1, 1349 GM Delft. 
+        Indien deze parkeerplaats vol blijkt te zijn, kunt u naar een van de andere garages op de campus. Hier kunt u op zaterdag en zondag gratis parkeren. 
+        Belangrijk is wel dat u een bankpas of OV-chipkaart nodig heeft om zich te registreren. Deze dient u tegen de slagboom aan te houden. 
+        Zolang u op momenten parkeert waarop gratis parkeren van toepassing is, zal er niets afgeschreven worden.
+      </v-card-text>
+      <br>
+      <br/>
+      <v-card-title>
+        Bowa's 
+      </v-card-title>
+      <v-card-text>
+        Als u aankomt met uw botenwagen bij de wedstrijd, neem dan contact op met +316 34366026 (Dianne Harting) of +316 47165273 (Koen de Regt). 
+        Let op dat het niet mogelijk is om tijdens de blokwisseling het terrein op te rijden of te verlaten met een botenwagen. 
+        Plan uw aankomst en vertrek zo dat het tijdens een blok is. 
+        U mag het terrein van Lijm en Cultuur betreden of verlaten met de botenwagen als de organisatie toestemming heeft gegeven.
+      </v-card-text>
+    </v-card>
     <v-card flat light>
       <v-card-title>
         Plattegrond Proteus-Eretes
