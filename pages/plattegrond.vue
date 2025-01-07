ed76890873166995becddfeb5292b304eccf34b8
WWK2024 <promo.winterwedstrijden@proteus-eretes.nl>
2025-01-07 18:57:36 +0100

    change_parkeren_07-01-2025

--- pages/plattegrond.vue
+++ pages/plattegrond.vue
@@ -9,6 +9,14 @@
         Indien deze parkeerplaats vol blijkt te zijn, kunt u naar een van de andere garages op de campus. Hier kunt u op zaterdag en zondag gratis parkeren. 
         Belangrijk is wel dat u een bankpas of OV-chipkaart nodig heeft om zich te registreren. Deze dient u tegen de slagboom aan te houden. 
         Zolang u op momenten parkeert waarop gratis parkeren van toepassing is, zal er niets afgeschreven worden.
+        <br>
+        Zie onderstaande link en afbeelding voor meer informatie:
+        <a href="https://www.tudelftcampus.nl/nl/bereikbaarheid/" target="_blank">TuDelftcampus.nl</a>
+        <br>
+        <img src="/BetalenTU.jpg" class="baankaart" style="width: 70%; height: 70%;"  />
+        <br>
+        <img src="/BetalenTU2.jpg" class="baankaart" style="width: 60%; height: 60%;" />
+
       </v-card-text>
       <br>
       <v-card-title>
