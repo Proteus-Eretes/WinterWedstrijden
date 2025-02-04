bc1bd2f8b7b4975e5e6e4f6f13e3158409eb92a7
WWK2024 <promo.winterwedstrijden@proteus-eretes.nl>
2025-02-04 10:57:04 +0100

    date

--- components/PoweredBy.vue
+++ components/PoweredBy.vue
@@ -8,7 +8,7 @@
       <!-- <span class="dot" style="background-color: LimeGreen">Groen</span> -->
       <span class="dot" style="background-color: green">Groen</span>
       <!-- <span class="dot" style="background-color: red">Rood</span> -->
-      Geüpdate: 03 feb 2025, 14:00
+      Geüpdate: 04 feb 2025, 11:00
     </v-card-text>
     
     <v-card-title>
