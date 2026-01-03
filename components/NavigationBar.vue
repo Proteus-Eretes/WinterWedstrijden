6112d4e7730ab08d122f857313afa65558aa6521
WWK2024 <promo.winterwedstrijden@proteus-eretes.nl>
2026-01-03 16:56:58 +0100

    Correctiefactoren is één woord

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -6,7 +6,7 @@
         <v-btn to="/" text width="100%" class="drawer--item">Nieuws</v-btn>
         <v-btn to="/blokken" text width="100%" class="drawer--item">Blokken</v-btn>
         <v-btn to="/bepalingen" text width="100%" class="drawer--item">Bepalingen</v-btn>
-        <v-btn to="/correctie factoren" text width="100%" class="drawer--item">Correctiefactoren</v-btn>
+        <v-btn to="/correctiefactoren" text width="100%" class="drawer--item">Correctiefactoren</v-btn>
         <v-btn to="/baan" text width="100%" class="drawer--item">Baankaart</v-btn>
         <v-btn to="/plattegrond" text width="100%" class="drawer--item">Parkeren & Plattegrond</v-btn>
         <v-btn to="/stuurlieden" text width="100%" class="drawer--item">Stuurlieden</v-btn>
@@ -76,7 +76,7 @@
               <v-btn to="/bepalingen" flat text>Bepalingen</v-btn>
             </v-list-item>
             <v-list-item>
-              <v-btn to="/correctie factoren" flat text>Correctiefactoren</v-btn>
+              <v-btn to="/correctiefactoren" flat text>Correctiefactoren</v-btn>
             </v-list-item>
             <v-list-item>
               <v-btn to="/baan" flat text>Baankaart</v-btn>
